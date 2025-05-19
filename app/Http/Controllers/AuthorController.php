ab5a0cae520d89f5b20648654b81d68b593a96f4
Deva muhammad putra <devamuhammad80@gmail.com>
2025-05-19 13:29:26 +0700

    Pesan commit kedua

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -9,6 +9,9 @@ class AuthorController extends Controller
     public function index()
     {
         $authors = Author::all();
-        return view('author.index', compact('authors'));
+    return response()->json([
+        'status' => 'success',
+        'data' => $authors
+    ]);
     }
 }
--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,8 +9,11 @@ class BookController extends Controller
     public function index()
     {
         // Ambil semua buku sekaligus relasi author
-        $books = Book::with('author')->get();
-        return view('books.index', compact('books'));
+       $books = Book::all();
+    return response()->json([
+        'status' => 'success',
+        'data' => $books
+    ]);
     }
 }
 
