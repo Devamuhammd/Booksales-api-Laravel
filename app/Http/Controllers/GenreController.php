5a6195c1930b19168b4fdcc8413ee86813da8ddc
Deva muhammad putra <devamuhammad80@gmail.com>
2025-05-21 09:54:33 +0700

    commit ketiga nih euy

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -3,16 +3,51 @@
 namespace App\Http\Controllers;
 
 use App\Models\Genre;
+use Illuminate\Support\Facades\Validator;
 
 class GenreController extends Controller
 {
     public function index()
     {
         $genres = Genre::all();
+
+        if (!$genres) {
+            return response()->json([
+            "success" => true,
+            "message" => "resource data not found!",
+            "data" => null
+        ], 200);
+        }
         return response()->json([
             "success" => true,
             "message" => "Get all resources",
             "data" => $genres
         ], 200);
+    }
+
+    public function store($request) {
+        //1. Validasi
+        $validator = Validator::make(request()->all(), [
+            "name" => "required|string",
+            "description" => "required|string"
+        ]);
+        //2. cek data yang eror
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+             ], 422);
+        }
+        //3. simpan data
+        $genre = Genre::create([
+            "name" => $request->name,
+            "description" => $request->description
+        ]);
+        //4. response berhasil
+        return response()->json([
+            "success" => true,
+            "message" => "resource created seccessfully",
+            "data" => $genre
+        ], 201);
     }
 }
--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Genre extends Model
 {
     protected $table = 'genres'; // tabel genres di database
+
+
+    protected $fillable = ['name', 'description'];
 }
